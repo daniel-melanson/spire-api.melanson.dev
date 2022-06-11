433e81ad281e839a622c59a833ccd221bcd63685
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-06-10 20:18:39 -0400

    fix: course and subject regexs and varchar sizes

--- spire/regexp.py
+++ spire/regexp.py
@@ -1,7 +1,7 @@
-SUBJECT_ID_REGEXP = r"(?P<subject_id>[A-Z\-]{3,9}[A-Z])"
-SUBJECT_TITLE_REGEXP = r"(?P<subject_title>[A-Za-z\- :&]{3,99}[a-z])"
+SUBJECT_ID_REGEXP = r"(?P<subject_id>[A-Z\-]{2,9}[A-Z])"
+SUBJECT_TITLE_REGEXP = r"(?P<subject_title>[A-Za-z\- :&]{2,99}[a-z])"
 
-COURSE_ID_NUM_REGEXP = r"(?P<course_number>[\w\d\-]{2,9}[\w\d])"
+COURSE_ID_NUM_REGEXP = r"(?P<course_number>[A-Za-z0-9\-]{1,9}[A-Za-z0-9])"
 COURSE_ID_REGEXP = SUBJECT_ID_REGEXP + r" " + COURSE_ID_NUM_REGEXP
 
 COURSE_TITLE_REGEXP = r"(?P<course_title>.{3,99})"
