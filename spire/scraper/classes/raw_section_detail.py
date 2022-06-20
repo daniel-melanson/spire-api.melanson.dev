8a9470ab8df39f6b41d796fb59633b73b7dcc042
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-06-19 21:11:39 -0400

    feat: course component normalization

--- spire/scraper/classes/raw_section_detail.py
+++ spire/scraper/classes/raw_section_detail.py
@@ -1,4 +1,5 @@
 from functools import reduce
+import re
 from typing import Optional
 
 from spire.models import Course, CourseDetail, Section, SectionDetail
@@ -7,12 +8,25 @@ from spire.scraper.shared import assert_dict_keys_subset
 
 from .shared import RawField, RawObject, key_override_factory, re_override_factory, to_camel_case
 
+def class_component_norm(x: str) -> list[str]:
+    x = x.replace("\n", " ")
+
+    l = []
+
+    for m in re.finditer(r"(Lecture|Laboratory|Individualized Study)( Required)?", x):
+        if m.group(2) is not None:
+            l.append(m.group(1) + " (Required)")
+        else:
+            l.append(m.group(1))
+
+    return l
+
 DETAILS = [
     RawField(k="Status", re=r"^(Open|Closed)$"),
     RawField(k="Class Number", re=r"^\d{3,10}$"),
     RawField(k="Session", normalizers=[key_override_factory({"*University": "University", "UWW": "University Without Walls" })]),
     RawField(k="Units", normalizers=[re_override_factory((r"^\d$", "$0.00"), (r"^(\d+) - (\d+)$", "$1.00 - $2.00"))], re=UNITS_REGEXP),
-    RawField(k="Class Components"),
+    RawField(k="Class Components", normalizers=[class_component_norm]),
     RawField(k="Career"),
     RawField(k="Grading"),
     RawField(k="Topic"),
