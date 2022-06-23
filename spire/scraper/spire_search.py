e5b9d5a483d7090d2e4611099db411608cfa0dc7
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-06-23 14:12:10 -0400

    refactor: use rawstaff

--- spire/scraper/spire_search.py
+++ spire/scraper/spire_search.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support.ui import Select
 from spire.models import SectionCoverage
 from spire.patterns import TERM_REGEXP
 from spire.scraper.classes.raw_course import RawCourse
+from spire.scraper.classes.raw_meeting_information import RawStaff
 from spire.scraper.classes.raw_section import RawSection
 from spire.scraper.timer import Timer
 
@@ -31,13 +32,13 @@ def _scrape_meeting_instructor_list(sections_table, link_id):
 
         instructor_list = []
         if len(raw_instructor_text) == 0:
-            instructor_list.append(("Staff", None))
+            instructor_list.append(RawStaff(name="Staff"))
         else:
             for email_link in instructor_column.find_elements(By.CSS_SELECTOR, "a[href^='mailto:']"):
                 href = email_link.get_property("href")
                 staff_name = email_link.text
                 staff_email = href[len("mailto:") :]
-                instructor_list.append((staff_name, staff_email))
+                instructor_list.append(RawStaff(name=staff_name, email=staff_email))
 
         meeting_instructor_list.append(instructor_list)
 
