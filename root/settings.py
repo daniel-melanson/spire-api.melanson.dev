a25f760a6b06c9a9ec1dc29fe6c8ed1cbd135e35
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-07-11 12:58:58 -0400

    chore: use DATABASE_URL

--- root/settings.py
+++ root/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
 
 def get_bool_env(key):
     if key in os.environ:
@@ -77,12 +79,7 @@ WSGI_APPLICATION = "root.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "spire",
-    }
-}
+DATABASES = {"default": dj_database_url.parse(os.getenv("DATABASE_URL"))}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
