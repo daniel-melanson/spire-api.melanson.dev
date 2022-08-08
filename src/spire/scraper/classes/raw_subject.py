79d8aa2d8699a033ea01489589cbd0a101eff09a
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-08-08 16:35:42 -0400

    chore: overrides

--- src/spire/scraper/classes/raw_subject.py
+++ src/spire/scraper/classes/raw_subject.py
@@ -16,6 +16,7 @@ SUBJECT_OVERRIDES = key_override_factory(
         "HT-MGT": ("HTM", "Hospitality & Tourism Management"),
         "LLEIP": ("LLIEP", "LL: Intensive English Program"),
         "LLSR&O": ("LLSRO", "LL: Sports, Recreation, and Outdoors"),
+        "LLC": ("LLC", "Languages, Literatures, and Cultures"),
         ("MI-ENG", "M&I-ENG"): ("MIE", "Mechanical & Industrial Engineering"),
         ("NEUROS&B", "NEUROSB"): ("NSB", "Neuroscience & Behavior"),
         ("ORG&EVBI", "ORGEVBI"): ("OEB", "Organismic & Evolutionary Biology"),
@@ -23,6 +24,7 @@ SUBJECT_OVERRIDES = key_override_factory(
         "SC@MIC": ("SC@MIC", "Science @ Mt. Ida College"),
         "SPHHS": ("SPHHS", "School of Public Health & Health Sciences"),
         "STOCKSCH": ("STOCKSCH", "Stockbridge School of Agriculture"),
+        "WGSS": ("Women, Gender, Sexuality Studies"),
         "ZEROCRED": ("TSTOCRED", "Zero Credit Test Equiv"),
     }
 )
