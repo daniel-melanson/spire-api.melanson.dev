b319be39f6cd86ed51c110b65e6d787dd3ae2dde
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-08-09 19:57:05 -0400

    fix: add setting

--- src/config/settings.py
+++ src/config/settings.py
@@ -116,6 +116,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # rest_framework
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
