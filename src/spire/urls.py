a11ad8c61cd14ac1876cc541e7525fd51cd867bc
daniel-melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-11-11 09:03:41 -0500

    fix: delete old imports

--- src/spire/urls.py
+++ src/spire/urls.py
@@ -12,7 +12,6 @@ from spire.views import (
     InstructorViewSet,
     SectionViewSet,
     SubjectViewSet,
-    TermEventViewSet,
     TermViewSet,
 )
 
@@ -23,35 +22,17 @@ router.register("buildings", BuildingViewSet, basename="building")
 router.register("building-rooms", BuildingRoomViewSet, basename="buildingroom")
 
 router.register("terms", TermViewSet, basename="term")
-# router.register("term-events", TermEventViewSet, basename="termevent")
 
 router.register("academic-groups", AcademicGroupViewSet, basename="academicgroup")
 
 router.register("subjects", SubjectViewSet, basename="subject")
 
 router.register("courses", CourseViewSet, basename="course")
-#router.register("course-detail", CourseDetailViewSet, basename="coursedetail")
-# router.register(
-#     "course-enrollment-information",
-#     CourseEnrollmentInformationViewSet,
-#     basename="courseenrollmentinformation",
-# )
 router.register("course-offerings", CourseOfferingViewSet, basename="courseoffering")
 
 router.register("instructors", InstructorViewSet, basename="instructor")
 
 router.register("sections", SectionViewSet, basename="section")
-# router.register("section-detail", SectionDetailViewSet, basename="sectiondetail")
-# router.register("section-availability", SectionAvailabilityViewSet, basename="sectionavailability")
-# router.register(
-#     "section-combined-availability",
-#     SectionCombinedAvailabilityViewSet,
-#     basename="sectioncombinedavailability",
-# )
-# router.register("section-restriction", SectionRestrictionViewSet, basename="sectionrestriction")
-# router.register(
-#     "section-meeting-information", SectionMeetingInformationViewSet, basename="sectionmeetinginformation"
-# )
 
 router.register("coverage", CoverageViewSet, basename="coverage")
 
