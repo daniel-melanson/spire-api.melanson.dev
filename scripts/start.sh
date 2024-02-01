c5badfd853ab984edd78bb791477d3e4c9ae2343
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2024-02-01 18:58:37 -0500

    fix: start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip3 install pipenv
 pipenv install
-cd ./src
-pipenv run python ./src/manage.py collectstatic
-pipenv run gunicorn -c python:src.config.gunicorn src.config.wsgi
+cd ./src/
+pipenv run python manage.py collectstatic
+pipenv run gunicorn -c python:config.gunicorn config.wsgi
