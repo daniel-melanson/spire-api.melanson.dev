356ae288e0134a6b174f6980fb0b84fcf661b272
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2024-04-24 23:50:42 -0400

    fix: docker compose -> docker-compose

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-docker compose down --remove-orphans
-docker system prune --all --force
-docker compose up --build --remove-orphans --detach --wait
+docker-compose down --remove-orphans
+docker-compose up --build --remove-orphans --detach --wait
